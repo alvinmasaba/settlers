b73eda4ad565dfe4fe76c055a678887ba2c6dade
funkyman3121 <alvinmasaba@gmail.com>
2023-05-19 16:52:46 -0400

    Initial springboot setup

---
+++ src/test/java/com/masaba/settlers/BoardGameApplicationTests.java
@@ -0,0 +1,8 @@
+package com.masaba.settlers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardGameApplicationTests {
+}
