791f51c35be86951af02dfae4cdc428d5e74cc0c
funkyman3121 <alvinmasaba@gmail.com>
2023-05-30 12:56:02 -0400

    Fixes indentation. Adds Edge class and class methods.

--- src/main/java/com/masaba/settlers/model/Edge.java
+++ src/main/java/com/masaba/settlers/model/Edge.java
@@ -1,6 +1,29 @@
 package com.masaba.settlers.model;
+
 import java.util.*;
 
 public class Edge {
-  
+    private List<Tile> neighbours;
+    private Road road;
+
+    public Edge() {
+        this.neighbours = new ArrayList<>();
+        this.road = null;
+    }
+
+    public List<Tile> getNeighbours() {
+        return neighbours;
+    }
+
+    public void addNeighbour(Tile tile) {
+        this.neighbours.add(tile);
+    }
+
+    public Road getRoad() {
+        return road;
+    }
+
+    public void setRoad(Road road) {
+        this.road = road;
+    } 
 }
--- src/main/java/com/masaba/settlers/model/Tile.java
+++ src/main/java/com/masaba/settlers/model/Tile.java
@@ -2,35 +2,35 @@ package com.masaba.settlers.model;
 import java.util.*;
 
 public class Tile {
-  private int row;
-  private int index;
-  private List<Tile> neighbours;
-  private Map<String, Edge> edges;
-  private Map<String, Vertex> vertices;
-  private List<List<Tile>> board;
+    private int row;
+    private int index;
+    private List<Tile> neighbours;
+    private Map<String, Edge> edges;
+    private Map<String, Vertex> vertices;
+    private List<List<Tile>> board;
 
 
-  public Tile(int row, int index, List<List<Tile>> board) {
-    this.row = row;
-    this.index = index;
-    this.board = board;
-    this.neighbours = findNeighbours();
-    this.edges = findOrCreateEdges();
-    this.vertices = findOrCreateVertices();
-  }
+    public Tile(int row, int index, List<List<Tile>> board) {
+        this.row = row;
+        this.index = index;
+        this.board = board;
+        this.neighbours = findNeighbours();
+        this.edges = findOrCreateEdges();
+        this.vertices = findOrCreateVertices();
+    }
 
-  private List<Tile> findNeighbours() {
-    // To do
-    return new ArrayList<>();
-  }
+    private List<Tile> findNeighbours() {
+        // To do
+        return new ArrayList<>();
+    }
 
-  private Map<String, Edge> findOrCreateEdges() {
-    // To do
-    return new HashMap<>();
-  }
+    private Map<String, Edge> findOrCreateEdges() {
+        // To do
+        return new HashMap<>();
+    }
 
-  private Map<String, Vertex> findOrCreateVertices() {
-    // To do
-    return new HashMap<>();
-  }
+    private Map<String, Vertex> findOrCreateVertices() {
+        // To do
+        return new HashMap<>();
+    }
 }
