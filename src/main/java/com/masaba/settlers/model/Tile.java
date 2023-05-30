c43a865bb13514dd8c1681ea96ae19b6d0400d65
funkyman3121 <alvinmasaba@gmail.com>
2023-05-30 14:22:47 -0400

    Writes Tile.findOrCreateEdges

--- src/main/java/com/masaba/settlers/model/Tile.java
+++ src/main/java/com/masaba/settlers/model/Tile.java
@@ -1,4 +1,5 @@
 package com.masaba.settlers.model;
+
 import java.util.*;
 
 public class Tile {
@@ -19,6 +20,7 @@ public class Tile {
         this.vertices = findOrCreateVertices();
     }
 
+
     private Map<String, Tile> findNeighbours(Tile[][] board, int row, int index) {
         Map<String, Tile> neighbours = new HashMap<>();
 
@@ -38,13 +40,46 @@ public class Tile {
         return neighbours;
     }
 
+
     private Map<String, Edge> findOrCreateEdges() {
-        // To do
-        return new HashMap<>();
+        Map<String, Edge> edges = new HashMap<>();
+
+        addEdgeFromNeighbour("top_left", "bottom_right", edges);
+        addEdgeFromNeighbour("top_right", "bottom_left", edges);
+        addEdgeFromNeighbour("right", "left", edges);
+        addEdgeFromNeighbour("bottom_right", "top_left", edges);
+        addEdgeFromNeighbour("bottom_left", "top_right", edges);
+        addEdgeFromNeighbour("left", "right", edges);
+
+        return edges;
     }
+
+
+    private void addEdgeFromNeighbour(String neighbourKey, String edgeKey, Map<String, Edge> edges) {
+        if (neighbours.get(neighbourKey) != null) {
+            Edge edge = neighbours.get(neighbourKey).getEdges().get(edgeKey);
+            if (edge == null) {
+                edge = new Edge();
+                edge.addNeighbour(this);
+                edge.addNeighbour(neighbours.get(neighbourKey));
+            }
+            edges.put(neighbourKey, edge);
+        }
+    }
+
 
     private Map<String, Vertex> findOrCreateVertices() {
         // To do
         return new HashMap<>();
     }
+
+
+    private Map<String, Edge> getEdges() {
+        return this.edges;
+    }
+
+    
+    private Map<String, Vertex> getVertices() {
+        return this.vertices;
+    } 
 }
